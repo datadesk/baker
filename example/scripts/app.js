4dd24580f878ed7cfb81ff851597f727ec7edf0d
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-10-31 17:04:56 -0700

    Add support for Svelte and Preact, remove old rollup.js

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -1,9 +1,22 @@
 import { intcomma } from 'journalize';
+import SvelteApp from './app.svelte';
+import { h, render } from 'preact';
 
 console.log(process.env.BAKER_DEMO_VAR);
 
+function PreactApp({ name }) {
+  return <h1>Hello {name}!</h1>;
+}
+
 function main() {
   console.log(intcomma(5432));
+
+  new SvelteApp({
+    target: document.querySelector('#svelte'),
+    props: { name: 'Svelte' },
+  });
+
+  render(<PreactApp name="Preact" />, document.querySelector('#preact'));
 }
 
 main();
