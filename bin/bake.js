5fc65e194c86b924656090712afae35f710a8ff2
James Perez <james.perez@latimes.com>
2024-10-31 12:37:21 -0700

    cleaning up baker.config handling

--- bin/bake.js
+++ bin/bake.js
@@ -7,7 +7,6 @@ import { resolve } from 'path';
 // packages
 import debug from 'debug';
 import mri from 'mri';
-import { rollup } from 'rollup';
 
 // local
 import { Baker } from '../lib/index.js';
@@ -41,70 +40,81 @@ const defaultConfig = {
   crosswalkPath: undefined,
 };
 
-function getDefaultFromConfig(module) {
-  return module.__esModule ? module.default : module;
+// function getDefaultFromConfig(module) {
+//   return module.__esModule ? module.default : module;
+// }
+
+// async function compileAndLoadConfig(pathToConfig) {
+//   const bundle = await rollup({
+//     external: () => true,
+//     input: pathToConfig,
+//     treeshake: false,
+//   });
+//
+//   const {
+//     output: [{ code }],
+//   } = await bundle.generate({
+//     exports: 'named',
+//     format: 'cjs',
+//     interop: 'auto',
+//   });
+//   const loadedConfig = requireFromString(code, pathToConfig);
+//
+//   return getDefaultFromConfig(loadedConfig);
+// }
+
+async function betterPrepareConfig(flags) {
+  const { input, config } = flags;
+  const projectConfigFilePath = resolve(input, !!config ? config : defaultConfigFile);
+  const projectConfig = await import(projectConfigFilePath);
+
+  return {
+    ...defaultConfig,
+    ...projectConfig
+  };
 }
 
-async function compileAndLoadConfig(pathToConfig) {
-  const bundle = await rollup({
-    external: () => true,
-    input: pathToConfig,
-    treeshake: false,
-  });
-
-  const {
-    output: [{ code }],
-  } = await bundle.generate({
-    exports: 'named',
-    format: 'cjs',
-    interop: 'auto',
-  });
-  const loadedConfig = await import(code, pathToConfig);
-
-  return getDefaultFromConfig(loadedConfig);
-}
-
-async function prepareConfig(inputOptions) {
-  // the input directory everything is relative to
-  const input = inputOptions.input;
-
-  // a config parameter was passed
-  if (inputOptions.config) {
-    // we check to see if it was passed as a boolean and use our default path to the config, otherwise we use what was given
-    const pathToConfig = resolve(
-      input,
-      inputOptions.config === true ? defaultConfigFile : inputOptions.config
-    );
-
-    inputOptions = await compileAndLoadConfig(pathToConfig);
-  }
-
-  // prep a helper function to resolve paths against input
-  const resolver = (key) => inputOptions[key] || defaultConfig[key];
-
-  const options = {};
-
-  options.assets = resolver('assets');
-  options.createPages = resolver('createPages');
-  options.data = resolver('data');
-  options.domain = resolver('domain');
-  options.embeds = resolver('embeds');
-  options.entrypoints = resolver('entrypoints');
-  options.imageSrcSizes = resolver('imageSrcSizes');
-  options.input = resolver('input');
-  options.layouts = resolver('layouts');
-  options.nunjucksVariables = resolver('nunjucksVariables');
-  options.nunjucksFilters = resolver('nunjucksFilters');
-  options.nunjucksTags = resolver('nunjucksTags');
-  options.minifyOptions = resolver('minifyOptions');
-  options.output = resolver('output');
-  options.pathPrefix = resolver('pathPrefix');
-  options.staticRoot = resolver('staticRoot');
-  options.svelteCompilerOptions = resolver('svelteCompilerOptions');
-  options.crosswalkPath = resolver('crosswalkPath');
-
-  return options;
-}
+// async function prepareConfig(inputOptions) {
+//   // the input directory everything is relative to
+//   const input = inputOptions.input;
+//
+//   // a config parameter was passed
+//   if (inputOptions.config) {
+//     // we check to see if it was passed as a boolean and use our default path to the config, otherwise we use what was given
+//     const pathToConfig = resolve(
+//       input,
+//       inputOptions.config === true ? defaultConfigFile : inputOptions.config
+//     );
+//
+//     inputOptions = await compileAndLoadConfig(pathToConfig);
+//   }
+//
+//   // prep a helper function to resolve paths against input
+//   const resolver = (key) => inputOptions[key] || defaultConfig[key];
+//
+//   const options = {};
+//
+//   options.assets = resolver('assets');
+//   options.createPages = resolver('createPages');
+//   options.data = resolver('data');
+//   options.domain = resolver('domain');
+//   options.embeds = resolver('embeds');
+//   options.entrypoints = resolver('entrypoints');
+//   options.imageSrcSizes = resolver('imageSrcSizes');
+//   options.input = resolver('input');
+//   options.layouts = resolver('layouts');
+//   options.nunjucksVariables = resolver('nunjucksVariables');
+//   options.nunjucksFilters = resolver('nunjucksFilters');
+//   options.nunjucksTags = resolver('nunjucksTags');
+//   options.minifyOptions = resolver('minifyOptions');
+//   options.output = resolver('output');
+//   options.pathPrefix = resolver('pathPrefix');
+//   options.staticRoot = resolver('staticRoot');
+//   options.svelteCompilerOptions = resolver('svelteCompilerOptions');
+//   options.crosswalkPath = resolver('crosswalkPath');
+//
+//   return options;
+// }
 
 const mriConfig = {
   alias: {
@@ -132,7 +142,7 @@ async function run(args) {
   const { _, ...flags } = mri(args, mriConfig);
 
   const command = _[0];
-  const config = await prepareConfig(flags);
+  const config = await betterPrepareConfig(flags);
 
   logger('command:', command);
   logger('resolved input flags:', config);
