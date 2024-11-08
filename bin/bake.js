9dacc4138f09e99f67e495a3a8d37d740f24cf09
James Perez <james.perez@latimes.com>
2024-11-07 16:37:16 -0800

    refactoring screenshots

--- bin/bake.js
+++ bin/bake.js
@@ -44,25 +44,6 @@ function getDefaultFromModule(module) {
   return module.__esModule ? module.default : module;
 }
 
-// async function compileAndLoadConfig(pathToConfig) {
-//   const bundle = await rollup({
-//     external: () => true,
-//     input: pathToConfig,
-//     treeshake: false,
-//   });
-//
-//   const {
-//     output: [{ code }],
-//   } = await bundle.generate({
-//     exports: 'named',
-//     format: 'cjs',
-//     interop: 'auto',
-//   });
-//   const loadedConfig = requireFromString(code, pathToConfig);
-//
-//   return getDefaultFromModule(loadedConfig);
-// }
-
 async function betterPrepareConfig(flags) {
   console.log('betterPrepareConfig:flags');
   console.log(JSON.stringify(flags, null, 3));
@@ -93,48 +74,6 @@ async function betterPrepareConfig(flags) {
 
   return mergedConfig;
 }
-
-// async function prepareConfig(inputOptions) {
-//   // the input directory everything is relative to
-//   const input = inputOptions.input;
-//
-//   // a config parameter was passed
-//   if (inputOptions.config) {
-//     // we check to see if it was passed as a boolean and use our default path to the config, otherwise we use what was given
-//     const pathToConfig = resolve(
-//       input,
-//       inputOptions.config === true ? defaultConfigFile : inputOptions.config
-//     );
-//
-//     inputOptions = await compileAndLoadConfig(pathToConfig);
-//   }
-//
-//   // prep a helper function to resolve paths against input
-//   const resolver = (key) => inputOptions[key] || defaultConfig[key];
-//
-//   const options = {};
-//
-//   options.assets = resolver('assets');
-//   options.createPages = resolver('createPages');
-//   options.data = resolver('data');
-//   options.domain = resolver('domain');
-//   options.embeds = resolver('embeds');
-//   options.entrypoints = resolver('entrypoints');
-//   options.imageSrcSizes = resolver('imageSrcSizes');
-//   options.input = resolver('input');
-//   options.layouts = resolver('layouts');
-//   options.nunjucksVariables = resolver('nunjucksVariables');
-//   options.nunjucksFilters = resolver('nunjucksFilters');
-//   options.nunjucksTags = resolver('nunjucksTags');
-//   options.minifyOptions = resolver('minifyOptions');
-//   options.output = resolver('output');
-//   options.pathPrefix = resolver('pathPrefix');
-//   options.staticRoot = resolver('staticRoot');
-//   options.svelteCompilerOptions = resolver('svelteCompilerOptions');
-//   options.crosswalkPath = resolver('crosswalkPath');
-//
-//   return options;
-// }
 
 const mriConfig = {
   alias: {
@@ -185,12 +124,8 @@ async function run(args) {
       }
       break;
     case 'screenshot':
-      // Change a few config options for taking screenshots
-      const screenshotConfig = { ...config, output: SCREENSHOT_DIR };
-      const screenshotBaker = new Baker(screenshotConfig);
-
       try {
-        await screenshotBaker.screenshot();
+        await baker.screenshots();
 
         console.log(green(bold('The screenshot was a success!')));
       } catch (err) {
